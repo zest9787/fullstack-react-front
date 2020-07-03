6bde491525b131641ac80e3b2326ea9e40be90ea
lucidman <lucidman.han@gmail.com>
2020-07-03 09:24:00 +0900

    Update commonAction.js
    임시저장

--- src/redux/common/commonAction.js
+++ src/redux/common/commonAction.js
@@ -4,3 +4,5 @@ export const addMenu = (menu) => ({type: 'ADD_MENU', menu});
 export const removeMenu = (id) => ({type: 'REMOVE_MENU', id});
 
 export const tabChange = (id) => ({type: 'TAB_CHANGE', id});
+
+export const toggleDrawer = () => ({type: 'TOGGLE_DRAWER'});
