26eac08b5114e61764ac4076769948d82f35e3fa
lucidman <lucidman.han@gmail.com>
2020-07-03 09:25:16 +0900

    Update userAction.js
    임시저장

--- src/redux/user/userAction.js
+++ src/redux/user/userAction.js
@@ -1,3 +1,4 @@
 export const loginRequest = data => ({type: 'LOGIN_REQUEST', data});
 export const loginSuccess = data => ({type: 'LOGIN_SUCCESS', data});
 export const loginFailure = data => ({type: 'LOGIN_FAILURE', data});
+export const logout = () => ({type:'LOGOUT_SUCCESS'});
